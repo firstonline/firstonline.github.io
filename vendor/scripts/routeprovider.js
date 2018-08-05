9486aa8c56d6dd87b425c080e946a9a936e66795
unknown <fffirstonline@gmail.com>
2018-08-05 16:11:50 +0100

    Testing controllers

--- vendor/scripts/routeprovider.js
+++ vendor/scripts/routeprovider.js
@@ -13,13 +13,13 @@ angular.module('portfolioApp', ['ngSanitize', 'ngRoute'])
     $locationProvider.html5Mode(false);
 }])
 .controller('MainCtrl', ['$route', '$routeParams', '$location',
-  function MainCtrl($route, $routeParams, $location) {
+  function MainCtrl($scope, $route, $routeParams, $location) {
 	$scope.name = "test";
 	$scope.$route = $route;
 	$scope.$location = $location;
 	$scope.$routeParams = $routeParams;
 }])
-.controller('ProjectCtrl', ['$routeParams', function BookCtrl($routeParams) {
+.controller('ProjectCtrl', ['$routeParams', function BookCtrl($scope, $routeParams) {
     $scope.title = 'ProjectCtrl';
     $scope.params = $routeParams;
 }])
