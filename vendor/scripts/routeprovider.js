e13efc9ec88ff959a46d5c9454472732fbae4b0b
unknown <fffirstonline@gmail.com>
2018-08-05 11:31:19 +0100

    Angular

---
+++ vendor/scripts/routeprovider.js
@@ -0,0 +1,34 @@
+angular.module('portfolioApp', ['ngSanitize', 'ngRoute'])
+.config(['$routeProvider', '$locationProvider',
+  function($routeProvider, $locationProvider) {
+    $routeProvider
+		.when("/", {
+			templateUrl : "portfolioPage.html"
+		})
+      .when('/Book/:bookId', {
+        templateUrl: 'book.html',
+        controller: 'BookCtrl',
+        controllerAs: 'book'
+      })
+      .when('/Book/:bookId/ch/:chapterId', {
+        templateUrl: 'chapter.html',
+        controller: 'ChapterCtrl',
+        controllerAs: 'chapter'
+      });
+
+    $locationProvider.html5Mode(true);
+}])
+.controller('MainCtrl', ['$route', '$routeParams', '$location',
+  function MainCtrl($route, $routeParams, $location) {
+    this.$route = $route;
+    this.$location = $location;
+    this.$routeParams = $routeParams;
+}])
+.controller('BookCtrl', ['$routeParams', function BookCtrl($routeParams) {
+  this.name = 'BookCtrl';
+  this.params = $routeParams;
+}])
+.controller('ChapterCtrl', ['$routeParams', function ChapterCtrl($routeParams) {
+  this.name = 'ChapterCtrl';
+  this.params = $routeParams;
+}]);
