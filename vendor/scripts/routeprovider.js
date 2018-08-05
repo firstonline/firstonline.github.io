e3836be7be1510c353fd74eb732cc71b9c674950
unknown <fffirstonline@gmail.com>
2018-08-05 15:47:00 +0100

    new routing for projects

--- vendor/scripts/routeprovider.js
+++ vendor/scripts/routeprovider.js
@@ -5,12 +5,10 @@ angular.module('portfolioApp', ['ngSanitize', 'ngRoute'])
 		.when("/", {
 			templateUrl : "portfolioPage.html"
 		})
-      .when('/project1', {
-        templateUrl: 'project1.html',
+      .when('/project/:projectId', {
+        templateUrl: 'project.html',
+		controller: 'ProjectCtrl',
       })
-      .when('/project2', {
-        templateUrl: 'project2.html',
-      });
 
     $locationProvider.html5Mode(false);
 }])
@@ -20,11 +18,7 @@ angular.module('portfolioApp', ['ngSanitize', 'ngRoute'])
     this.$location = $location;
     this.$routeParams = $routeParams;
 }])
-.controller('BookCtrl', ['$routeParams', function BookCtrl($routeParams) {
-  this.name = 'BookCtrl';
+.controller('ProjectCtrl', ['$routeParams', function BookCtrl($routeParams) {
+  this.title = 'ProjectCtrl';
   this.params = $routeParams;
 }])
-.controller('ChapterCtrl', ['$routeParams', function ChapterCtrl($routeParams) {
-  this.name = 'ChapterCtrl';
-  this.params = $routeParams;
-}]);
